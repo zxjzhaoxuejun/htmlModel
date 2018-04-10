b4f622d276b17237fbc6fcf81be860c4b1ba3728
邢贵志 <xinggz@zhongan.com.cn>
2018-04-10 14:43:51 +0800

    Init

---
+++ fis-conf.js
@@ -0,0 +1,19 @@
+fis.match('*', {
+    release: false
+})
+
+fis.match('**/(*.less)', {
+    rExt: '.css',
+    parser: fis.plugin('less-2.x'),
+    release: 'css/$1'
+});
+
+fis.match('/src/pug/(*.pug)', {
+    rExt: '.html',
+    parser: fis.plugin('pug'),
+    release: '$1'
+});
+
+fis.match('/src/js/(*.js)', {
+    release: 'js/$1'
+});
