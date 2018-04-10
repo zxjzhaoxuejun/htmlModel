d02d3114e3a769503cf6adad51d2e9a94c0a6353
邢贵志 <xinggz@zhongan.com.cn>
2018-04-10 17:11:31 +0800

    添加autoprefixer和sass

--- fis-conf.js
+++ fis-conf.js
@@ -14,6 +14,17 @@ fis.match('/src/less/(*.less)', {
     release: 'css/$1'
 });
 
+fis.match('/src/scss/(*.scss)', {
+    rExt: '.css',
+    preprocessor: fis.plugin('autoprefixer', {
+        browsers: ['Chrome < 29']
+    }),
+    parser: fis.plugin('node-sass', {
+        outputStyle: 'expanded'
+    }),
+    release: 'css/$1'
+});
+
 fis.match('/src/pug/(*.pug)', {
     rExt: '.html',
     parser: fis.plugin('pug'),
